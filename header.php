44c62dc8680f9f6778bcce9c315e78b5fe1e10c5
Patrick Thomas <patrick@patrickwthomas.net>
2021-11-27 23:51:14 +0000

    Start adding bulma theming

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,39 +23,58 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'galena' ); ?></a>
+	<?php wp_body_open(); ?>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
+	<nav id="site-navigation" class="navbar" role="navigation" aria-label="main navigation">
+		<div class="navbar-brand">
+			<a class="navbar-item" href="https://bulma.io">
+				<img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+			</a>
+
+			<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+			</a>
+		</div>
+
+		<div id="navbarBasicExample" class="navbar-menu">
+			<div class="navbar-start">
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+						'menu_class' => 'navbar-item'
+					)
+				);
+				?>
+			</div>
+		</div>
+		<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'galena'); ?></button> -->
+	</nav><!-- #site-navigation -->
+
+	<header id="masthead" class="site-header hero is-primary">
+		<div class="hero-body site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
+			if (is_front_page() && is_home()) :
+			?>
+				<h1 class="site-title title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+			<?php
 			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
+			?>
+				<p class="site-title title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+			<?php
 			endif;
-			$galena_description = get_bloginfo( 'description', 'display' );
-			if ( $galena_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $galena_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			$galena_description = get_bloginfo('description', 'display');
+			if ($galena_description || is_customize_preview()) :
+			?>
+				<p class="site-description subtitle"><?php echo $galena_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+																							?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'galena' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
+
+	<section id="page" class="site section">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'galena'); ?></a>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file
  *
@@ -15,21 +16,21 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
+<main id="primary" class="site-main">
+	<div class="container">
 		<?php
-		if ( have_posts() ) :
+		if (have_posts()) :
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
+			if (is_home() && !is_front_page()) :
+		?>
 				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					<h1 class="page-title title screen-reader-text"><?php single_post_title(); ?></h1>
 				</header>
-				<?php
+		<?php
 			endif;
 
 			/* Start the Loop */
-			while ( have_posts() ) :
+			while (have_posts()) :
 				the_post();
 
 				/*
@@ -37,7 +38,7 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part('template-parts/content', get_post_type());
 
 			endwhile;
 
@@ -45,12 +46,12 @@ get_header();
 
 		else :
 
-			get_template_part( 'template-parts/content', 'none' );
+			get_template_part('template-parts/content', 'none');
 
 		endif;
 		?>
-
-	</main><!-- #main -->
+	</div>
+</main><!-- #main -->
 
 <?php
 get_sidebar();
