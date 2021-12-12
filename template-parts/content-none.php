622dd65f356651f4560745947f24cb3bb64c573e
Patrick Thomas <patrick@patrickwthomas.net>
2021-12-12 01:59:40 +0000

    Formatting, change primary colors

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Template part for displaying a message that posts cannot be found
  *
@@ -7,46 +6,41 @@
  *
  * @package Galena
  */
-
 ?>
 
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title title"><?php esc_html_e('Nothing Found', 'galena'); ?></h1>
+		<h1 class="page-title title"><?php esc_html_e("Nothing Found", "galena"); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
-		<?php
-		if (is_home() && current_user_can('publish_posts')) :
+		<?php if (is_home() && current_user_can("publish_posts")):
+    printf(
+      "<p>" .
+        wp_kses(
+          /* translators: 1: link to WP admin new post page. */
+          __('Ready to publish your first post? <a href="%1$s">Get started here</a>.', "galena"),
+          [
+            "a" => [
+              "href" => [],
+            ],
+          ]
+        ) .
+        "</p>",
+      esc_url(admin_url("post-new.php"))
+    );
+  elseif (is_search()): ?>
 
-			printf(
-				'<p>' . wp_kses(
-					/* translators: 1: link to WP admin new post page. */
-					__('Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'galena'),
-					array(
-						'a' => array(
-							'href' => array(),
-						),
-					)
-				) . '</p>',
-				esc_url(admin_url('post-new.php'))
-			);
+			<p><?php esc_html_e(
+     "Sorry, but nothing matched your search terms. Please try again with some different keywords.",
+     "galena"
+   ); ?></p>
+		<?php get_search_form();else: ?>
 
-		elseif (is_search()) :
-		?>
-
-			<p><?php esc_html_e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'galena'); ?></p>
-		<?php
-			get_search_form();
-
-		else :
-		?>
-
-			<p><?php esc_html_e('It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'galena'); ?></p>
-		<?php
-			get_search_form();
-
-		endif;
-		?>
+			<p><?php esc_html_e(
+     "It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.",
+     "galena"
+   ); ?></p>
+		<?php get_search_form();endif; ?>
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -6,19 +6,21 @@
  *
  * @package Galena
  */
-
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php the_title(
+    sprintf('<h2 class="entry-title title"><a href="%s" rel="bookmark">', esc_url(get_permalink())),
+    "</a></h2>"
+  ); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
+		<?php if ("post" === get_post_type()): ?>
 		<div class="entry-meta">
 			<?php
-			galena_posted_on();
-			galena_posted_by();
-			?>
+   galena_posted_on();
+   galena_posted_by();
+   ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
